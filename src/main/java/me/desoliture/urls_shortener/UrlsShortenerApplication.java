7849428fae9281da3e59cf6af949109317ba4f48
desoliture <desoliture@evoplay.tech>
2024-10-09 16:22:30 +0300

    Setup project

---
+++ src/main/java/me/desoliture/urls_shortener/UrlsShortenerApplication.java
@@ -0,0 +1,13 @@
+package me.desoliture.urls_shortener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlsShortenerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UrlsShortenerApplication.class, args);
+    }
+
+}
